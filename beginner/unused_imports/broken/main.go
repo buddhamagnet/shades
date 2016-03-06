1fb97debddd97ebcf4c394d80905a68854468f24
buddhamagnet <buddhamagnet@gmail.com>
2016-03-06 18:23:58 +0000

    unused import woes complete

---
+++ beginner/unused_imports/broken/main.go
@@ -0,0 +1,9 @@
+package main
+
+// broken LOL - goimports prevents me from saving this,
+// just trust me, the compiler will complain if you add
+// and import statement and don't use it.
+
+func main() {
+
+}
